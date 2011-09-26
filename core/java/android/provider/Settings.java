224bc4e756d617b0a03c662331631fed27c49842
Adam Green <greena88@gmail.com>
2011-09-26 01:42:23 +0100

    Added battery percentage

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1821,6 +1821,9 @@ public final class Settings {
         /** @hide */
         public static final String EXPANDED_FLASH_MODE = "expanded_flash_mode";
 
+        /** @hide */
+        public static final String BATTERY_PERCENTAGE = "battery_percentage";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
